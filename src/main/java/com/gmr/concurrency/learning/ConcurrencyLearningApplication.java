7fa9edac736000680b615944b060dfb82a383153
guomeiran <guomeiran130@163.com>
2018-12-24 11:16:44 +0800

    init

---
+++ src/main/java/com/gmr/concurrency/learning/ConcurrencyLearningApplication.java
@@ -0,0 +1,14 @@
+package com.gmr.concurrency.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyLearningApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gmr/concurrency/learning/ConcurrencyLearningApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gmr.concurrency.learning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
